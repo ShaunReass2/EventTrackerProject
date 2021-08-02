dd47d6a4d77385260c428baf540f29ffe4840768
Shaun R <shaun.reass@icloud.com>
2021-08-02 10:48:43 -0500

    ec2 deployed

--- JustRide/src/main/java/com/skilldistillery/motorcycles/JustRideApplication.java
+++ JustRide/src/main/java/com/skilldistillery/motorcycles/JustRideApplication.java
@@ -2,9 +2,16 @@ package com.skilldistillery.motorcycles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JustRideApplication {
+public class JustRideApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(JustRideApplication.class);
+	  }
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(JustRideApplication.class, args);
